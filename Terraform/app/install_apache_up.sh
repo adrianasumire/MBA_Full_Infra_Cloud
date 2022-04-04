150ba14e46eb08e8398ee9ff8192694393777dde
Adriana Sumire <adriana.sumire@gmail.com>
2022-04-04 11:01:55 -0300

    - Alterando para a porta 80

--- Terraform/app/install_apache_up.sh
+++ Terraform/app/install_apache_up.sh
@@ -12,9 +12,10 @@ echo "Instalar o Apache2"
 apt install -y apache2
 echo "Copiar o ${dirApp}/apache2.service ${dirServ}/."
 cp ${dirApp}/apache2.service ${dirServ}/.
-echo "Renomear o ${dirApa}/ports.conf ${dirApa}/ports.conf.orig"
-mv ${dirApa}/ports.conf ${dirApa}/ports.conf.orig
-echo "Copiar o ${dirApp}/apache2.service ${dirServ}/."
-cp ${dirApp}/ports.conf ${dirApa}/.
+# Alterava para a porta 8080
+#echo "Renomear o ${dirApa}/ports.conf ${dirApa}/ports.conf.orig"
+#mv ${dirApa}/ports.conf ${dirApa}/ports.conf.orig
+#echo "Copiar o cp ${dirApp}/ports.conf ${dirApa}/"
+#cp ${dirApp}/ports.conf ${dirApa}/.
 apachectl stop
 service apache2 start
